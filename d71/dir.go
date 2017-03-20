c2b03b014f6ba5c4efad9ed1ec5efa2e5fe0e4e6
Mike McGann <mike.mcgann@blackchip.org>
2017-03-20 18:48:10 -0400

    some docs

--- d71/dir.go
+++ d71/dir.go
@@ -7,35 +7,36 @@ import (
 type FileType int
 
 const (
-	Del FileType = iota
-	Seq
-	Prg
-	Usr
-	Rel
+	Del FileType = iota // Deleted
+	Seq                 // Sequential
+	Prg                 // Program
+	Usr                 // User
+	Rel                 // Relative
 )
 
 type FileInfo struct {
-	Type   FileType
-	SaveAt bool
-	Locked bool
-	Splat  bool
-	Name   string
-	Size   int
-	Track  int
-	Sector int
+	Type   FileType //
+	SaveAt bool     // SAVE-@ operation
+	Locked bool     //
+	Splat  bool     // True if the file wasn't properly closed
+	Name   string   //
+	Size   int      // Number of sectors
+	Track  int      // Location of first
+	Sector int      // Location of first
 }
 
 type dirWalker struct {
 	e          *Editor
-	nextTrack  int
-	nextSector int
-	entry      int
+	nextTrack  int // Track of the next directory block or $00
+	nextSector int // Sector of the next directory block
+	entry      int // Entry (0-7) in this sector that the walker is at
 }
 
 func newDirWalker(d Disk) *dirWalker {
 	w := &dirWalker{e: d.Editor()}
 	w.e.Seek(DirTrack, 0, 0)
 
+	// BAM sector contains the location of the first directory block
 	firstTrack := w.e.Read()
 	firstSector := w.e.Read()
 	w.e.Seek(firstTrack, firstSector, 0)
@@ -43,12 +44,16 @@ func newDirWalker(d Disk) *dirWalker {
 	return w
 }
 
+// Remember the link for the next directory block
 func (w *dirWalker) readLink() {
 	e := w.e.Mark()
 	w.nextTrack = e.Read()
 	w.nextSector = e.Read()
 }
 
+// Advance the walker to the next directory entry. If at the last entry
+// for this sector, move to the next sector or return false indicating
+// the end of the listing.
 func (w *dirWalker) advance() bool {
 	w.entry++
 	if w.entry == 8 {
@@ -68,17 +73,21 @@ func (w *dirWalker) next() (*FileInfo, bool) {
 	for {
 		e := w.e.Mark().Move(2)
 		ftype := e.Read()
+		// Skip over this entry if it has been deleted
 		if ftype == 0 {
 			ok := w.advance()
 			if !ok {
+				// Reached the end, no more entries
 				return nil, false
 			}
 		} else {
+			// If not deleted, this is a valid entry.
 			break
 		}
 	}
 
 	fi := &FileInfo{}
+
 	w.e.Move(2)
 	ftype := w.e.Read()
 	fi.Type = FileType(ftype & 0x7)
@@ -90,5 +99,6 @@ func (w *dirWalker) next() (*FileInfo, bool) {
 	fi.Name = strings.Trim(w.e.ReadString(16), "\xa0")
 	w.e.Move(0x1e - 0x15)
 	fi.Size = w.e.ReadWord()
+
 	return fi, true
 }
